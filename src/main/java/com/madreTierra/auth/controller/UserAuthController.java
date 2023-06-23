69f9e4b78e1539221e5bfb83ded38ec442507863
agustin-kiryk <agustin.kiryk@gmail.com>
2023-06-23 00:11:58 -0300

    auth_login

--- src/main/java/com/madreTierra/auth/controller/UserAuthController.java
+++ src/main/java/com/madreTierra/auth/controller/UserAuthController.java
@@ -1,4 +1,48 @@
 package com.madreTierra.auth.controller;
 
+import com.madreTierra.auth.dto.*;
+import com.madreTierra.auth.service.UserDetailsCustomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("https://consorcio-production.up.railway.app")
+
+
 public class UserAuthController {
+
+    @Autowired
+    private UserDetailsCustomService userDetailsServices;
+
+
+
+
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseUserDto> signUp(@Valid @RequestBody RequestUserDto user) {
+        ResponseUserDto userRegister = this.userDetailsServices.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRegister);
+    }
+
+    //Todo terminar este endpoint auth/registerAdmin, crea el admin
+    @PostMapping("/registerAdmin")
+    public ResponseEntity<ResponseUserDto> signUpAdmin(@Valid @RequestBody RequestUserDto user) {
+        ResponseUserDto userRegister = this.userDetailsServices.saveAdmin(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRegister);
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> signIn(@RequestBody AuthenticationRequest authenticationRequest) {
+
+        AuthenticationResponse response = userDetailsServices.signIn(authenticationRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+
 }
--- src/main/java/com/madreTierra/repository/RoleRepository.java
+++ src/main/java/com/madreTierra/repository/RoleRepository.java
@@ -1,9 +1,12 @@
 package com.madreTierra.repository;
 
 import com.madreTierra.entity.RoleEntity;
+import com.madreTierra.enumeration.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity,Long> {
+    RoleEntity findByRoleName(RoleName roleName);
 }
+
