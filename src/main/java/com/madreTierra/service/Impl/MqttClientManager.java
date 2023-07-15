209681cf528922bdd40c7ab14fe20d041af08dbe
agustin-kiryk <agustin.kiryk@gmail.com>
2023-07-15 16:34:04 -0300

    test PATHCERT

--- src/main/java/com/madreTierra/service/Impl/MqttClientManager.java
+++ src/main/java/com/madreTierra/service/Impl/MqttClientManager.java
@@ -42,10 +42,10 @@ public class MqttClientManager {
     MachineRepository machineRepository;
     @Autowired
     TransactionRepository transactionRepository;
-    static String certPath = "8210489b5e-certificate_pem.crt";
-    static String keyPath = "8210489b5e-private_pem.key";
+    static String certPath = "src/main/resources/8210489b5e-certificate_pem.crt";
+    static String keyPath = "src/main/resources/8210489b5e-private_pem.key";
     static String caPath = "src/main/resources/AmazonRootCA1.pem";
-    static String clientId = "230517_11";
+    static String clientId = "230517_85";
     static String endpoint = "a2i1cbvebks9le-ats.iot.us-west-1.amazonaws.com";
     static int port = Integer.parseInt(("8883"));
     static String proxyHost = String.valueOf(Integer.parseInt("0"));
