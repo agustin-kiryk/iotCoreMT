1650ccc0c6458068b529466f69313d43ef27be27
agustin-kiryk <agustin.kiryk@gmail.com>
2023-07-15 18:20:55 -0300

    test PATHCERT

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
 FROM openjdk:11
 MAINTAINER DSI
 COPY target/demo-0.0.1-SNAPSHOT.jar iot-core-mt-app.jar
+# Crea una carpeta para los certificados y copia los archivos dentro del contenedor
+RUN mkdir /certs
+COPY 8210489b5e-certificate_pem.crt /certs/
+COPY 8210489b5e-private_pem.key /certs/
+COPY AmazonRootCA1.pem /certs/
+
+# Define la variable de entorno para indicar la ubicación de los certificados en el contenedor
+ENV CERT_PATH certs/8210489b5e-certificate_pem.crt
+ENV KEY_PATH certs/8210489b5e-private_pem.key
+ENV CA_PATH certs/AmazonRootCA1.pem
 ENTRYPOINT ["java", "-jar", "/iot-core-mt-app.jar"]
--- src/main/java/com/madreTierra/service/Impl/MqttClientManager.java
+++ src/main/java/com/madreTierra/service/Impl/MqttClientManager.java
@@ -42,9 +42,9 @@ public class MqttClientManager {
     MachineRepository machineRepository;
     @Autowired
     TransactionRepository transactionRepository;
-    static String certPath = "https://github.com/agustin-kiryk/iot-core-mt/blob/f7333ed9700acc7760da0f75bed38efaf71e73c3/8210489b5e-certificate_pem.crt";
-    static String keyPath = "https://github.com/agustin-kiryk/iot-core-mt/blob/f7333ed9700acc7760da0f75bed38efaf71e73c3/8210489b5e-private_pem.key";
-    static String caPath = "https://github.com/agustin-kiryk/iot-core-mt/blob/f7333ed9700acc7760da0f75bed38efaf71e73c3/src/main/resources/AmazonRootCA1.pem";
+    static String certPath = "certs/8210489b5e-certificate_pem.crt";
+    static String keyPath = "certs/8210489b5e-private_pem.key";
+    static String caPath = "certs/AmazonRootCA1.pem";
     static String clientId = "230517_85";
     static String endpoint = "a2i1cbvebks9le-ats.iot.us-west-1.amazonaws.com";
     static int port = Integer.parseInt(("8883"));
