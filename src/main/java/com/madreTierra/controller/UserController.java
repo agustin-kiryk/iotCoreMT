52fee2f2ff7d9ef17e5d39ee7a4fa43ed2b4dc45
agustin-kiryk <agustin.kiryk@gmail.com>
2023-07-19 20:52:43 -0300

    feature/controllers cors

--- src/main/java/com/madreTierra/controller/UserController.java
+++ src/main/java/com/madreTierra/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.madreTierra.controller;
 
+import com.madreTierra.dto.MachineDTO;
 import com.madreTierra.dto.UserDTO;
 import com.madreTierra.service.IUserService;
+import com.madreTierra.service.Impl.MachineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,8 @@ import java.util.List;
 public class UserController {
     @Autowired
     IUserService userService;
+    @Autowired
+    MachineService machineService;
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserId(@PathVariable Long id){
         UserDTO response = userService.getUserId(id);
@@ -25,6 +29,10 @@ public class UserController {
     {
         List<UserDTO> users= userService.listAllUsers();
         return ResponseEntity.ok().body(users);
+    }
+    @GetMapping("/allMachines")
+    public ResponseEntity<List<MachineDTO>> getAllMachines(){
+        return ResponseEntity.ok().body(machineService.getAllMachines());
     }
 
 
