fc15d9927b67c30d5f1b357782c006e520881171
agustin-kiryk <agustin.kiryk@gmail.com>
2023-08-06 19:30:21 -0300

    widjets clocks

--- src/main/java/com/madreTierra/entity/StatePayEntity.java
+++ src/main/java/com/madreTierra/entity/StatePayEntity.java
@@ -1,9 +1,13 @@
 package com.madreTierra.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "STATUS_PAY")
 public class StatePayEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PAY_ID")
+    private Long id;
+
 }
